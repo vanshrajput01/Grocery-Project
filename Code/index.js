02109885e27f3abfa788e113bb01f03a4b157074
vanshrajput01 <rajputvansh009@gmail.com>
2023-12-13 09:34:10 +0530

    some more update in website

--- Code/index.js
+++ Code/index.js
@@ -17,6 +17,7 @@ function myFunction() {
     navWithLogo.style.display = "flex"
     navWithLogo.style.border = "1.5px solid rgb(240, 240, 240)"
     navWithLogo.style.backgroundColor = "rgb(260, 260, 260)"
+    navWithLogo.style.zIndex = "1"
 
     
     
